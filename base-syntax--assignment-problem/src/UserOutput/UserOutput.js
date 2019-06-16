fb0593d252b06a961360d6aa516798697b48064a
Astrid E Hamre <astridehamre@outlook.com>
2019-06-16 10:34:41 +0200

    Initial commit

---
+++ base-syntax--assignment-problem/src/UserOutput/UserOutput.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+
+import './UserOutput.css'; 
+
+const userOutput = (props) =>{
+    return (
+        <div className="UserOutput">
+        <p> Username: {props.userName}</p>
+        <p>How are you?</p>
+        </div>
+    );
+
+};
+
+export default userOutput; 
